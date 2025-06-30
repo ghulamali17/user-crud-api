636cd641a141fdfeecc82b9dd1e9c3df71f6573c
Ghulam Ali <132977700+ghulamali17@users.noreply.github.com>
2025-06-30 22:41:34 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+require("dotenv").config();
+
 
 dotenv.config();
 connectDB();
